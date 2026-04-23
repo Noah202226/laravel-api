db0388f0cd60293e2a746d3e5a50abf7ccd681bf
Noa <noaligpitan26@gmail.com>
2026-04-23 14:27:46 +0800

    checking app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/health-check', function () {
+    try {
+        
+        return response()->json(['status' => 'online', "server_time" => now()->toDateTimeString(), "message" => "The server is healthy and connected to the database."]);
+    } catch (\Exception $e) {
+        return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+    }
 });
